e267eb57ba8b0f6631de2f29791feb606ceb9570
Sven Hübner <huebner.sven@gmx.de>
2012-09-16 14:00:22 +0200

    Model-Übersetzungen hinzugefügt

--- app/admin/static_pages.rb
+++ app/admin/static_pages.rb
@@ -1,17 +1,31 @@
 ActiveAdmin.register StaticPage do
   config.filters = false
   config.sort_order = "heading_asc"
+  actions :all, except: [:destroy]
+
+  index do
+    column :heading
+    column :text
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 
   show title: :title do |page|
     attributes_table do 
       row :heading
       row :text
-      table_for page.paragraphs do
-        column :order do |paragraph|
-          paragraph.heading
-        end
-        column :text do |paragraph|
-          paragraph.text
+      unless page.paragraphs.empty?
+        table_for page.paragraphs do
+          column :order do |paragraph|
+            paragraph.order
+          end
+          column :heading do |paragraph|
+            paragraph.heading
+          end
+          column :text do |paragraph|
+            paragraph.text
+          end
         end
       end
     end
@@ -19,13 +33,13 @@ ActiveAdmin.register StaticPage do
 
 
   form do |f|
-    f.inputs "Seite" do
+    f.inputs :static_page do
       f.input :heading
       f.input :text
     end
 
     f.has_many :paragraphs do |p_f|
-      p_f.inputs "Abschnitt" do
+      p_f.inputs :paragraph do
         if !p_f.object.nil?
           p_f.input :_destroy, as: :boolean, label: "Entfernen?"
         end
