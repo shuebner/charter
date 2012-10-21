b2953009399fcd9e6ad289b6c248e962a161b4d5
Sven Hübner <huebner.sven@gmx.de>
2012-10-21 09:26:02 +0200

    org: Settings-Tabelle aus schema.rb entfernt

--- db/schema.rb
+++ db/schema.rb
@@ -150,17 +150,6 @@ ActiveRecord::Schema.define(:version => 20121020144144) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "settings", :force => true do |t|
-    t.string   "var",                      :null => false
-    t.text     "value"
-    t.integer  "thing_id"
-    t.string   "thing_type", :limit => 30
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
-  end
-
-  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
-
   create_table "static_pages", :force => true do |t|
     t.string   "slug",         :limit => 30,  :null => false
     t.string   "title",        :limit => 100, :null => false
