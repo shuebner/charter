b9782cb244ba4354b2ce3d83efb16eb27deec46d
Sven Hübner <huebner.sven@gmx.de>
2012-10-24 11:28:31 +0200

    sehr große Bilder werden vor dem Speichern verkleinert

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -18,4 +18,22 @@ describe Image do
       image.attachment_uid.should_not be_blank
     end
   end
+
+  describe "when image" do
+    describe "is very large" do
+      let!(:large_image) do
+        create(:image, attachment: File.new("/home/sven/Bilder/too_big.jpg"))
+      end
+      it "should be resized to a standard maximum size" do
+        large_image.attachment.width.should <= 1600
+        large_image.attachment.height.should <= 1200
+      end
+    end
+    describe "is small enough" do
+      it "should not be resized" do
+        image.attachment.width.should == 563
+        image.attachment.height.should == 750
+      end
+    end
+  end
 end
