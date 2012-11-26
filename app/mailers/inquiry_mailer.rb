0f46848c6532f3718fbeefbdc8bf645fe28dfb6a
Sven Hübner <huebner.sven@gmx.de>
2012-11-26 18:05:01 +0100

    Mailer für Törnanfragen hinzugefügt

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,9 +1,16 @@
+# encoding: utf-8
 class InquiryMailer < ActionMailer::Base
   default from: 'mailer@palve-charter.de'
   default to: 'anfragen@palve-charter.de'
 
   def general_inquiry(inquiry)
     @inquiry = inquiry
-    mail(subject: "Allgemeine Anfrage von #{inquiry.first_name} #{inquiry.last_name}")
+    mail(subject: "Allgemeine Anfrage von #{inquiry.full_name}")
+  end
+
+  def trip_inquiry(inquiry)
+    @inquiry = inquiry
+    trip_name = inquiry.trip_date.display_name_with_trip
+    mail(subject: "Törnanfrage zu #{trip_name} von #{inquiry.full_name}")
   end
 end
