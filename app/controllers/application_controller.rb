3440d1545ef2e04bd9cc5e4627e73d09e8b6d993
Sven Hübner <huebner.sven@gmx.de>
2012-12-03 15:29:36 +0100

    Passwortschutz der Seite entfernt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate
-
   protected
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == "gast" && password == "test"
-      end
-    end
-  end
 
   def not_found
     raise ActionController::RoutingError.new('Not found')
