4ea63331068da3079346faab7c6d6bd21e920e34
Sven Hübner <huebner.sven@gmx.de>
2012-12-14 18:25:18 +0100

    Date Picker für Schiffsanfrage genutzt

--- spec/requests/boat_inquiries_spec.rb
+++ spec/requests/boat_inquiries_spec.rb
@@ -14,16 +14,8 @@ describe "BoatInquiries" do
       visit new_boat_inquiry_path(boat: boat.slug)
     end
     @additional_form_actions = lambda do |page|
-      begin_date = Date.new(2013, 7, 1)
-      end_date = Date.new(2013, 7, 7)
-
-      page.select begin_date.day.to_s, from: "boat_inquiry_begin_date_3i"
-      page.select I18n.t("date.month_names")[begin_date.month], from: "boat_inquiry_begin_date_2i"
-      page.select begin_date.year.to_s, from: "boat_inquiry_begin_date_1i"
-
-      page.select end_date.day.to_s, from: "boat_inquiry_end_date_3i"
-      page.select I18n.t("date.month_names")[end_date.month], from: "boat_inquiry_end_date_2i"
-      page.select end_date.year.to_s, from: "boat_inquiry_end_date_1i"
+      page.fill_in "boat_inquiry_begin_date", with: "01.07.2013"
+      page.fill_in "boat_inquiry_end_date", with: "07.07.2013"
       
       page.select 2.to_s, from: "boat_inquiry_adults"
       page.select 1.to_s, from: "boat_inquiry_children"
