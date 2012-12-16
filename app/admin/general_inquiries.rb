d133fc2f3841d2b9a1277dee608e65b482bed33f
Sven Hübner <huebner.sven@gmx.de>
2012-12-16 13:55:28 +0100

    Anfragen zum Adminbereich hinzugefügt

---
+++ app/admin/general_inquiries.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register GeneralInquiry do
+  menu parent: I18n.t("activerecord.models.inquiry.other")
+
+  config.sort_order = 'created_at_desc'
+
+  filter :last_name
+  filter :email
+  filter :created_at  
+  
+  actions :all, except: [:edit]
+
+  index do
+    column :created_at
+    column :full_name, sortable: :last_name
+    column :email
+    column(:text) { |i| i.text.length <= 50 ? i.text : "#{i.text.to(50)}..." }
+    default_actions
+  end
+
+  show do |i|
+    attributes_table_for i do
+      row :created_at
+      row :first_name
+      row :last_name
+      row :email
+      row :text
+    end
+  end
+end
---
+++ app/admin/trip_inquiries.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register TripInquiry do
+  menu parent: I18n.t("activerecord.models.inquiry.other")
+
+  config.sort_order = 'created_at_desc'
+
+  filter :last_name
+  filter :email
+  filter :created_at
+  
+  actions :all, except: [:edit]
+
+  index do
+    column :created_at
+    column(:trip_date) { |i| i.trip_date.display_name_with_trip }
+    column :full_name
+    column :email
+    default_actions
+  end
+
+  show do |i|
+    attributes_table_for i do
+      row :created_at
+      row :trip_date
+      row :bunks
+      row :first_name
+      row :last_name
+      row :email
+      row :text
+    end
+  end
+end
