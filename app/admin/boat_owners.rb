4109330f1b1677af43f9f5510e99057e9923cc27
Sven Hübner <huebner.sven@gmx.de>
2012-12-16 15:18:13 +0100

    Schiffseigner im Admin-Bereich hinzugefügt

---
+++ app/admin/boat_owners.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register BoatOwner do
+  menu parent: I18n.t("boat_data")
+
+  filter :name
+
+  index do
+    column :name
+    column(:is_self) do |o|
+      status_tag (o.is_self ? "ja" : "nein"),
+        (o.is_self ? :ok : :error)
+    end
+  end
+
+  show do |o|
+    attributes_table do
+      row :name
+      row(:is_self) do
+        status_tag (o.is_self ? "ja" : "nein"),
+          (o.is_self ? :ok : :error)
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs :name, :is_self
+    f.actions
+  end
+end
